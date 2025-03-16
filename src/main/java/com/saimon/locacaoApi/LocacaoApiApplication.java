8fce85d9338ff9e0dbcc2d9ee4fe75f879e51ccc
Felipe Saimon <felipesaimon.lima@gmail.com>
2025-03-16 08:42:15 -0300

    Initial commit

---
+++ src/main/java/com/saimon/locacaoApi/LocacaoApiApplication.java
@@ -0,0 +1,13 @@
+package com.saimon.locacaoApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocacaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocacaoApiApplication.class, args);
+	}
+
+}
